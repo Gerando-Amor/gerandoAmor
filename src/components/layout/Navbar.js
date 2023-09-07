09510a99633b9acbd3c9986b8a3dfcaa4120d179
Wesley Willian <wesleywilliancg@gmail.com>
2023-09-06 23:40:57 -0300

    Incluindo indicador de página na navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,34 +1,66 @@
-import { Link } from "react-router-dom";
-
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import Container from "./Container";
-
 import styles from "./Navbar.module.css";
-import logo from "../img/Logo.png"
+import logo from "../img/Logo.png";
+import classNames from "classnames";
 
 function Navbar() {
+  const location = useLocation();
+
   return (
     <nav className={styles.navbar}>
       <Container>
         <Link className={styles.navBarr} to="/">
-            <div>
-                <img className={styles.img} src={logo} alt="Logo" /> 
-            </div>
-        <div>
-            <h1>Gerando <span>Amor</span></h1>
-        </div>
+          <div>
+            <img className={styles.img} src={logo} alt="Logo" />
+          </div>
+          <div>
+            <h1>
+              Gerando <span>Amor</span>
+            </h1>
+          </div>
         </Link>
         <ul className={styles.list}>
           <li className={styles.item}>
-            <Link to="/">Home</Link>
+            <Link
+              to="/"
+              className={classNames(styles.link, {
+                [styles.activeLink]: location.pathname === "/",
+              })}
+            >
+              Home
+            </Link>
           </li>
           <li className={styles.item}>
-            <Link to="/Projects">Contato</Link>
+            <Link
+              to="/Projects"
+              className={classNames(styles.link, {
+                [styles.activeLink]: location.pathname === "/Projects",
+              })}
+            >
+              Contato
+            </Link>
           </li>
           <li className={styles.item}>
-            <Link to="/contact">Sobre</Link>
+            <Link
+              to="/contact"
+              className={classNames(styles.link, {
+                [styles.activeLink]: location.pathname === "/contact",
+              })}
+            >
+              Sobre
+            </Link>
           </li>
           <li className={styles.item}>
-            <Link to="/company">Localização</Link>
+            <Link
+              to="/company"
+              className={classNames(styles.link, {
+                [styles.activeLink]: location.pathname === "/company",
+              })}
+            >
+              Localização
+            </Link>
           </li>
         </ul>
       </Container>
