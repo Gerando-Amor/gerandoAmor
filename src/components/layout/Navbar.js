2b6e061b41e77527e8a83ff412bcad8222899554
Crispim-Rodrigues <crispim.rodrigues.oficial@gmail.com>
2023-09-08 08:20:49 -0300

    Responsividade do header com estrutura condicional

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,18 +2,37 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import Container from "./Container";
 import styles from "./Navbar.module.css";
-import logo from "../img/Logo.png";
+import logo from "../img/Logo1.png";
 import classNames from "classnames";
+import { useState, useEffect } from 'react';
 
 function Navbar() {
   const location = useLocation();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const isDesktop = windowWidth >= 1023;
+
+
+  useEffect(() => {
+    // Atualize a largura da janela quando a janela for redimensionada
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      // Remova o ouvinte de redimensionamento quando o componente for desmontado
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <nav className={styles.navbar}>
       <Container>
-        <Link className={styles.navBarr} to="/">
+
+        <Link className={styles.navIcon} to="/">
           <div>
-            <img className={styles.img} src={logo} alt="Logo" />
+            <img className={styles.img} src={logo} alt="Logo"/>
           </div>
           <div>
             <h1>
@@ -21,48 +40,58 @@ function Navbar() {
             </h1>
           </div>
         </Link>
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <Link
-              to="/"
-              className={classNames(styles.link, {
-                [styles.activeLink]: location.pathname === "/",
-              })}
-            >
-              Home
-            </Link>
-          </li>
-          <li className={styles.item}>
-            <Link
-              to="/projects"
-              className={classNames(styles.link, {
-                [styles.activeLink]: location.pathname === "/projects",
-              })}
-            >
-              Projetos
-            </Link>
-          </li>
-          <li className={styles.item}>
-            <Link
-              to="/contact"
-              className={classNames(styles.link, {
-                [styles.activeLink]: location.pathname === "/contact",
-              })}
-            >
-              Contato
-            </Link>
-          </li>
-          <li className={styles.item}>
-            <Link
-              to="/location"
-              className={classNames(styles.link, {
-                [styles.activeLink]: location.pathname === "/location",
-              })}
-            >
-              Localização
-            </Link>
-          </li>
-        </ul>
+        {
+          isDesktop?
+
+            (
+              <ul className={styles.list}>
+                <li className={styles.item}>
+                  <Link
+                    to="/"
+                    className={classNames(styles.link, {
+                      [styles.activeLink]: location.pathname === "/",
+                    })}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className={styles.item}>
+                  <Link
+                    to="/projects"
+                    className={classNames(styles.link, {
+                      [styles.activeLink]: location.pathname === "/projects",
+                    })}
+                  >
+                    Projetos
+                  </Link>
+                </li>
+                <li className={styles.item}>
+                  <Link
+                    to="/contact"
+                    className={classNames(styles.link, {
+                      [styles.activeLink]: location.pathname === "/contact",
+                    })}
+                  >
+                    Contato
+                  </Link>
+                </li>
+                <li className={styles.item}>
+                  <Link
+                    to="/location"
+                    className={classNames(styles.link, {
+                      [styles.activeLink]: location.pathname === "/location",
+                    })}
+                  >
+                    Localização
+                  </Link>
+                </li>
+              </ul>
+            )
+            :
+            (
+              <div></div>
+            )
+        }
       </Container>
     </nav>
   );
