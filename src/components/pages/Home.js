e5ee9c65fe24cdf9d236c3807b6c6331b4aecd22
Wesley Willian <wesleywilliancg@gmail.com>
2023-10-03 11:56:04 -0300

    Imagem da home com detalhes curvados - inicio

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,18 @@
- 
+import React from "react";
+import styles from "./Home.module.css";
+
+
 
 function Home() {
-    return(
+    return (
         <section>
-            
-            
+            <div className={styles.container_caixa}>
+                <div className={styles.caixa}>
+                    <div className={styles.caixa_item_container}>
+                        <div className={styles.caixa_item}></div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
