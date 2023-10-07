7d855f887bcb6fd4a3df99a2be589649989fe90e
Crispim-Rodrigues <crispim.rodrigues.oficial@gmail.com>
2023-10-07 14:53:59 -0300

    section 2 ajeitada

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,9 +9,9 @@ import Seta from "../img/chevron-down.svg";
 
 function Home() {
   return (
-    <section>
-      <section className={styles.section}>
-        <div className={styles.container}>
+    <main>
+      <section className={styles.section_banner}>
+        <div className={styles.container_banner}>
           <div className={classNames(styles.boxtext, styles.w50)}>
             <h1>Juntos Fazemos a Diferença</h1>
             <h3>
@@ -20,7 +20,7 @@ function Home() {
               Criando Esperança
             </h3>
             <h4>Seja parte mudança. Junte-se a nós hoje!</h4>
-            <Link to="/comoajudar#doe">
+            <Link to="/comoajudar#doe" className={styles.button}>
               <button>Doar!</button>
             </Link>
           </div>
@@ -32,46 +32,40 @@ function Home() {
         </div>
       </section>
 
-      <section className={styles.container2}>
-        <div className={classNames(styles.w50, styles.quemsomos)}>
-          <h1>Quem Somos</h1>
-          <p>
-            A ONG "Gerando Amor" é uma organização <br />
-            dedicada a fazer a diferença na vida de crianças
-            <br />
-            carentes por meio de assistência social e espiritual.
-            <br />
-            Nosso principal objetivo é fornecer um ambiente <br />
-            acolhedor e apoiador para as crianças, onde não
-            <br /> apenas suas necessidades básicas são atendidas, mas <br />
-            também onde elas têm a oportunidade de aprender e<br />
-            crescer em diferentes aspectos de suas vidas.
-          </p>
-        </div>
-        <div className={classNames(styles.w50, styles.quemsomos, styles.imagequemsomos)}>
-          <img src={ImageQuemSomos1} alt="" srcset="" />
-        </div>
-        
+      <section className={styles.section_description}>
+        <div className={styles.container_description}>
+          <div className={classNames(styles.w50, styles.boxtext_description)}>
+            <h1>Quem Somos</h1>
+            <p>
+              A ONG "Gerando Amor" é uma organização dedicada a fazer a diferença na vida de crianças carentes por meio de assistência social e espiritual. 
+              Nosso principal objetivo é fornecer um ambiente acolhedor e apoiador para as crianças, 
+              onde não apenas suas necessidades básicas são atendidas, 
+              mas também onde elas têm a oportunidade de aprender e crescer em diferentes aspectos de suas vidas.
+            </p>
+          </div>
+          <div className={classNames(styles.w50, styles.imagequemsomos)}>
+            <img src={ImageQuemSomos1} alt="" srcset="" />
+          </div>
 
-        <div className={classNames(styles.w100, styles.containertranformamos)} >
-        <div className={classNames(styles.w50, styles.quemsomos2, styles.imagetransformamos)}>
-          <img src={ImageQuemSomos2} alt="" srcset="" />
-        </div>
-        <div className={classNames(styles.w50, styles.quemsomos2)}>
-          <h1>Como transformamos</h1>
-          <p>
-            Por meio do estabelecimento de vínculos de confiança que
-            <br /> contemplam conhecer a história de vida de cada criança,
-            <br /> adolescente e respectivas famílias que integram a comunidade
-            e<br /> entorno, asseguramos diálogo, relações de afetividade,
-            ampliação
-            <br /> do universo de troca de bens culturais, acesso à tecnologia e
-            a<br /> experimentação.
-          </p>
-        </div>
+
+          <div className={classNames(styles.w100, styles.containertranformamos)} >
+            <div className={classNames(styles.w50, styles.quemsomos2, styles.imagetransformamos)}>
+              <img src={ImageQuemSomos2} alt="" srcset="" />
+            </div>
+            <div className={classNames(styles.w50, styles.boxtext_description)}>
+              <h1>Como transformamos</h1>
+              <p>
+                Por meio do estabelecimento de vínculos de confiança que contemplam conhecer a história de vida de cada criança, 
+                adolescente e respectivas famílias que integram a comunidade e entorno, 
+                asseguramos diálogo, relações de afetividade, 
+                ampliação do universo de troca de bens culturais, 
+                acesso à tecnologia e a experimentação.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
-    </section>
+    </main>
   );
 }
 
