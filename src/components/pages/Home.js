a2cb9064edf7fa52f2889ef7c13cacc238a5b1cc
Crispim-Rodrigues <crispim.rodrigues.oficial@gmail.com>
2023-10-11 23:57:17 -0300

    create modular SectionDescription

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,51 +1,16 @@
 import React from "react";
 import styles from "./Home.module.css";
-import classNames from "classnames";
-import ImageQuemSomos1 from "../img/quemsomos1.jpg";
-import ImageQuemSomos2 from "../img/quemsomos2.png";
 import SwiperTeam from "./partials_home/SwiperTeam";
-import SectionHome from "./partials_home/SectionBanner";
+import SectionBanner from "./partials_home/SectionBanner";
+import SectionDescription from "./partials_home/SectionDescription";
 
 function Home() {
   return (
     <main>
-      <SectionHome/>
-    
+      <SectionBanner/>
+      <SectionDescription/>
 
-      {/*section description*/}
-      <section className={styles.section_description}>
-        <div className={styles.container_description}>
-          <div className={classNames(styles.w50, styles.boxtext_description)}>
-            <h1>Quem Somos</h1>
-            <p>
-              A ONG "Gerando Amor" é uma organização dedicada a fazer a diferença na vida de crianças carentes por meio de assistência social e espiritual.
-              Nosso principal objetivo é fornecer um ambiente acolhedor e apoiador para as crianças,
-              onde não apenas suas necessidades básicas são atendidas,
-              mas também onde elas têm a oportunidade de aprender e crescer em diferentes aspectos de suas vidas.
-            </p>
-          </div>
-          <div className={classNames(styles.w50, styles.imagequemsomos)}>
-            <img src={ImageQuemSomos1} alt="" srcset="" />
-          </div>
-
-
-          <div className={classNames(styles.w100, styles.containertranformamos)} >
-            <div className={classNames(styles.w50, styles.quemsomos2, styles.imagetransformamos)}>
-              <img src={ImageQuemSomos2} alt="" srcset="" />
-            </div>
-            <div className={classNames(styles.w50, styles.boxtext_description)}>
-              <h1>Como transformamos</h1>
-              <p>
-                Por meio do estabelecimento de vínculos de confiança que contemplam conhecer a história de vida de cada criança,
-                adolescente e respectivas famílias que integram a comunidade e entorno,
-                asseguramos diálogo, relações de afetividade,
-                ampliação do universo de troca de bens culturais,
-                acesso à tecnologia e a experimentação.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
+      
 
       {/* section team */}
       <section className={styles.section_team}>
