67c2a7814acb208a3f90d57eab2a73b9228bc96b
Crispim-Rodrigues <crispim.rodrigues.oficial@gmail.com>
2023-10-11 23:37:46 -0300

    create modular SectionBanner

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,38 +1,16 @@
 import React from "react";
 import styles from "./Home.module.css";
 import classNames from "classnames";
-import { Link } from "react-router-dom";
-import ImageInicio from "../img/foto.png";
 import ImageQuemSomos1 from "../img/quemsomos1.jpg";
 import ImageQuemSomos2 from "../img/quemsomos2.png";
-import Seta from "../img/chevron-down.svg";
 import SwiperTeam from "./partials_home/SwiperTeam";
-import '../../index.css'
+import SectionHome from "./partials_home/SectionBanner";
+
 function Home() {
   return (
     <main>
-      {/* section banner */}
-      <section className={styles.section_banner}>
-        <div className={styles.container_banner}>
-          <div className={classNames(styles.boxtext, styles.w50)}>
-            <h1>Juntos Fazemos a Diferença</h1>
-            <h3>
-              Transformando Vidas, Espalhando Amor,
-              <br />
-              Criando Esperança
-            </h3>
-            <h4>Seja parte mudança. Junte-se a nós hoje!</h4>
-            <Link to="/comoajudar#doe" className={styles.button}>
-              <button>Doar!</button>
-            </Link>
-          </div>
-          <div className={classNames(styles.w50, styles.boximage)}>
-            <div className={styles.caixa_item}></div>
-            <img src={ImageInicio} alt="" srcset="" />
-          </div>
-          <img className={styles.seta} src={Seta} alt="" srcset="" />
-        </div>
-      </section>
+      <SectionHome/>
+    
 
       {/*section description*/}
       <section className={styles.section_description}>
