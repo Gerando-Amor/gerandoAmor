ec0bc5c75d3232aecee854e27f63e39c89748ad5
Churros <klebersonluiz5@gmail.com>
2023-11-02 11:56:08 -0300

    Pagina "Projetos"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, RouterProvider } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Navbar from "./components/layout/Navbar";
+import Projects from "./components/pages/Projects";
 
 
 
@@ -11,6 +12,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="Projects" element={<Projects/>}/>
         </Routes>
       </Router>
       
