d2b94d115b3a2ba0badb05a4c93710908112e6f3
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2023-11-12 10:25:42 -0300

    add new componet

---
+++ src/components/pages/partials_Doe/SectionImgAss.js
@@ -0,0 +1,14 @@
+//Import Style
+import style from "./SectionImgAss.module.css"
+
+
+function SectionImgAss () {
+
+    return{
+      <section>
+        <div className={style.BackgroundImg}></div>
+      </section>
+    };
+};
+
+export default SectionImgAss
