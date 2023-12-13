56a62025ab4da5018d0d3ca65a98093eb9d30ccf
Crispim-Rodrigues <crispim.rodrigues.oficial@gmail.com>
2023-12-13 10:44:24 -0300

    Section BCFAQ Add

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,12 @@
 import SectionBanner from "./partials_contact/SectionBanner"
 import SectionMap from "./partials_contact/SectionMap"
+import SectionBFAQ from"./partials_contact/SectionBFAQ"
 function Contact() {
     return (
         <main>
             <SectionBanner/>
             <SectionMap/>
+            <SectionBFAQ/>
         </main>
     )
 
