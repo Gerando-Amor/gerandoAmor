5ab1efac24cc223fadae35cdee1de37d1728ab90
Crispim-Rodrigues <crispim.rodrigues.oficial@gmail.com>
2023-12-13 11:02:34 -0300

    Section fa1

---
+++ src/components/pages/partials_contact/SectionRFAQ.js
@@ -0,0 +1,10 @@
+import styles from "./SectionRFAQ.module.css"
+import React from 'react'
+export default function SectionRFAQ() {
+  return (
+    <section className={styles.SectionBanner}>
+        <div className={styles.BannerContainer}>
+        </div>
+    </section>
+  )
+}
