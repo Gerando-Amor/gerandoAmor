d81cc83db2ed3c5b35b043071df32e3fabcb52e7
Crispim-Rodrigues <crispim.rodrigues.oficial@gmail.com>
2023-12-26 20:37:18 -0300

    ajustes de responsividade

--- src/components/pages/partials_projects/sectionProjects.js
+++ src/components/pages/partials_projects/sectionProjects.js
@@ -3,51 +3,46 @@ import classNames from "classnames";
 
 import styles from "../partials_projects/sectionProjects.module.css"
 
-import Caratê from "../../img/Caratê.png";
+import Carate from "../../img/Carate.png";
 
-function SectionProjects(){
+function SectionProjects() {
 
 
-    return(
-        <section className={classNames(styles.section_projects, )}>
+    return (
+        <section className={classNames(styles.section_projects,)}>
 
             <div className={classNames(styles.container_projects)}>
-            <div className={styles.divh1_projects}>
-                <h1>Nossos Projetos</h1>
-            </div>
-            <div className={styles.cards_projects}>
-                <div className={classNames(styles.w100, styles.description_projects)}>
-                    <h1>Caratê</h1>
-                    <p>Com o esporte mudamos vidas e motivamos <br/> as pessoas se movimentarem</p>
+                <div className={styles.divh1_projects}>
+                    <h1>Nossos Projetos</h1>
                 </div>
-                <div className={classNames(styles.w100, styles.img_projects )}>
-                <img  src={Caratê} alt="" />
-                </div>
-            </div>
 
-
-
-            <div className={styles.cards_projects}>
-                <div className={classNames(styles.w100, styles.description_projects)}>
-                    <h1>Caratê</h1>
-                    <p>Com o esporte mudamos vidas e motivamos <br/> as pessoas se movimentarem</p>
+                <div className={styles.cards_projects}>
+                    <div className={classNames(styles.w100, styles.description_projects)}>
+                        <h1>Caratê</h1>
+                        <p>Com o esporte mudamos vidas e motivamos <br /> as pessoas se movimentarem</p>
+                    </div>
+                    <div className={classNames(styles.w100, styles.img_projects)}>
+                        <img src={Carate} alt="" />
+                    </div>
                 </div>
-                <div className={classNames(styles.w100, styles.img_projects )}>
-                <img  src={Caratê} alt="" />
+                <div className={styles.cards_projects}>
+                    <div className={classNames(styles.w100, styles.description_projects)}>
+                        <h1>Caratê</h1>
+                        <p>Com o esporte mudamos vidas e motivamos <br /> as pessoas se movimentarem</p>
+                    </div>
+                    <div className={classNames(styles.w100, styles.img_projects)}>
+                        <img src={Carate} alt="" />
+                    </div>
                 </div>
-            </div>
-
-
-            
-            <div className={styles.cards_projects}>
-                <div className={classNames(styles.w100, styles.description_projects)}>
-                    <h1>Caratê</h1>
-                    <p>Com o esporte mudamos vidas e motivamos <br/> as pessoas se movimentarem</p>
+                <div className={styles.cards_projects}>
+                    <div className={classNames(styles.w100, styles.description_projects)}>
+                        <h1>Caratê</h1>
+                        <p>Com o esporte mudamos vidas e motivamos <br /> as pessoas se movimentarem</p>
+                    </div>
+                    <div className={classNames(styles.w100, styles.img_projects)}>
+                        <img src={Carate} alt="" />
+                    </div>
                 </div>
-                <div className={classNames(styles.w100, styles.img_projects  )}>
-                <img  src={Caratê} alt="" />
-                </div>
-            </div>
             </div>
         </section>
     )
