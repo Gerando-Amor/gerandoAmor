54a594a30ce0408f90b7cb1de384c06afacaf55f
Crispim-Rodrigues <crispim.rodrigues.oficial@gmail.com>
2023-12-26 22:37:12 -0300

    Map

---
+++ src/components/pages/partials_projects/boxProject.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import classNames from 'classnames'
+import styles from './boxProject.module.css'
+import { useState, useEffect } from 'react';
+
+export default function BoxProject({Data}) {
+    const [imagem, setImagem] = useState(null)
+
+    useEffect(() => {
+        // Função para carregar dinamicamente a imagem
+        const carregarImagem = async () => {
+            try {
+                const imagemImportada = await import(`../../img/${Data.img}.png`);
+                setImagem(imagemImportada.default);
+            } catch (error) {
+                console.error(`Erro ao carregar a imagem: ${error}`);
+            }
+        };
+
+        // Chama a função para carregar a imagem ao montar o componente
+        carregarImagem();
+    }, [Data.id]);
+    return (
+        <div className={styles.cards_projects}>
+            <div className={classNames(styles.w100, styles.description_projects)}>
+                <h1>{Data.tittle}</h1>
+                <p>{Data.description}</p>
+            </div>
+            <div className={classNames(styles.w100, styles.img_projects)}>
+                <img src={imagem} alt="" />
+            </div>
+        </div>
+    )
+}
--- src/components/pages/partials_projects/sectionProjects.js
+++ src/components/pages/partials_projects/sectionProjects.js
@@ -1,48 +1,23 @@
-
 import classNames from "classnames";
-
-import styles from "../partials_projects/sectionProjects.module.css"
-
-import Carate from "../../img/Carate.png";
+import styles from "./sectionProjects.module.css";
+import Project from "./boxProject"
+import Data from "../../../Data/ProjectsData"
 
 function SectionProjects() {
 
 
     return (
         <section className={classNames(styles.section_projects,)}>
-
             <div className={classNames(styles.container_projects)}>
                 <div className={styles.divh1_projects}>
                     <h1>Nossos Projetos</h1>
                 </div>
+                {
+                    Data.map((Data) => (
+                        <Project Data={Data} />
+                    ))
+                }
 
-                <div className={styles.cards_projects}>
-                    <div className={classNames(styles.w100, styles.description_projects)}>
-                        <h1>Caratê</h1>
-                        <p>Com o esporte mudamos vidas e motivamos <br /> as pessoas se movimentarem</p>
-                    </div>
-                    <div className={classNames(styles.w100, styles.img_projects)}>
-                        <img src={Carate} alt="" />
-                    </div>
-                </div>
-                <div className={styles.cards_projects}>
-                    <div className={classNames(styles.w100, styles.description_projects)}>
-                        <h1>Caratê</h1>
-                        <p>Com o esporte mudamos vidas e motivamos <br /> as pessoas se movimentarem</p>
-                    </div>
-                    <div className={classNames(styles.w100, styles.img_projects)}>
-                        <img src={Carate} alt="" />
-                    </div>
-                </div>
-                <div className={styles.cards_projects}>
-                    <div className={classNames(styles.w100, styles.description_projects)}>
-                        <h1>Caratê</h1>
-                        <p>Com o esporte mudamos vidas e motivamos <br /> as pessoas se movimentarem</p>
-                    </div>
-                    <div className={classNames(styles.w100, styles.img_projects)}>
-                        <img src={Carate} alt="" />
-                    </div>
-                </div>
             </div>
         </section>
     )
